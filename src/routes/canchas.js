d9a41c8a357ea759c6d44985c898e779b94526dc
José Nájera <jsnajera1017@gmail.com>
2025-08-01 17:57:17 -0600

    Created models and routes for every requirement

---
+++ src/routes/canchas.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dataModel = require('../models/cancha');
+
+//Creación del Router
+const canchasRouter = express.Router();
+
+//Validación de campos correctos al crear usuario
+const validateBodyArgs = (req, res, next) => {
+    const { nombre, ubicacion, tipo} = req.body;
+    if (nombre && ubicacion && tipo) {
+      return next();
+    }
+    const error = new Error('Invalidate arguments nombre, ubicacion and tipo are required');
+    error.status = 400;
+    next(error);
+};
+
+//Get para obtener la lista de usuarios
+canchasRouter.get('/', async (req, res, next) => {
+try {
+    const result = await dataModel.getAllCourts();
+    res.status(200).json(result);
+} catch (err) {
+    next(err);
+}
+    return res;
+});
+
+//Post para crear un usuario
+canchasRouter.post('/', validateBodyArgs, async (req, res, next) => {
+try {
+    const result = await dataModel.createCourt(req.body);
+    res.status(200).json(result);
+} catch (err) {
+    next(err);
+}
+    return res;
+});
+
+module.exports = canchasRouter;
---
+++ src/routes/users.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dataModel = require('../models/user');
+
+//Creación del Router
+const usersRouter = express.Router();
+
+//Validación de campos correctos al crear usuario
+const validateBodyArgs = (req, res, next) => {
+    const { nombre, correo, telefono} = req.body;
+    if (nombre && correo && telefono) {
+      return next();
+    }
+    const error = new Error('Invalidate arguments nombre, correo and telefono are required');
+    error.status = 400;
+    next(error);
+};
+
+//Get para obtener la lista de usuarios
+usersRouter.get('/', async (req, res, next) => {
+try {
+    const result = await dataModel.getAllUsers();
+    res.status(200).json(result);
+} catch (err) {
+    next(err);
+}
+    return res;
+});
+
+//Post para crear un usuario
+usersRouter.post('/', validateBodyArgs, async (req, res, next) => {
+try {
+    const result = await dataModel.createUser(req.body);
+    res.status(200).json(result);
+} catch (err) {
+    next(err);
+}
+    return res;
+});
+
+module.exports = usersRouter;
